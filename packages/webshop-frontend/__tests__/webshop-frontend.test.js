f49b77bfc9b93f5c993f5ad6360e6968533f6c1d
ariel <arielmalada@outlook.com>
2024-01-07 16:24:38 +0700

    feat: frontend solid

--- packages/webshop-frontend/__tests__/webshop-frontend.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const webshopFrontend = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(webshopFrontend(), 'Hello from webshopFrontend');
-console.info('webshopFrontend tests passed');
--- packages/webshop-frontend/lib/webshop-frontend.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = webshopFrontend;
-
-function webshopFrontend() {
-  return 'Hello from webshopFrontend';
-}
